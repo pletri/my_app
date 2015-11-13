750cf63e874e635120eea426c05bd4ec9748a2fc
pletri <pletri@hotmail.com>
2015-11-13 12:28:36 -0500

    test 4

--- www/hello.js
+++ www/hello.js
@@ -5,27 +5,42 @@ var page = tabris.create("Page", {
   topLevel: true
 });
 
-var button = tabris.create("Button", {
-  text: "Native Widgets",
+var button1 = tabris.create("Button", {
+  text: "Native Widgets 1",
+  layoutData: {centerX: 0, top: 10}
+}).appendTo(page);
+
+var button2 = tabris.create("Button", {
+  text: "Native Widgets 2",
   layoutData: {centerX: 0, top: 100}
 }).appendTo(page);
 
 var label = tabris.create("TextView", {
   font: "24px",
-  layoutData: {centerX: 0, top: [button, 50]}
+  layoutData: {centerX: 0, top: [button2, 50]}
 }).appendTo(page);
 
-button.on("select", function() {
+
+button1.on("select", function() {
   WifiWizard.getCurrentSSID(ssidHandler, fail);
+//  label.set("text", "Totally Rock!");
+});
+
+button2.on("select", function() {
+  WifiWizard.getScanResults(listHandler, fail);
 //  label.set("text", "Totally Rock!");
 });
 
 page.open();
 
 function ssidHandler(s) {
-  labet.set("Current SSID : " + s);
+  label.set("text", "Current SSID : " + s);
 }
 
 function fail(e) {
-  label.set("ERROR : " + e);
+  label.set("text", "ERROR : " + e);
+}
+
+function listHandler(s) {
+  label.set("text", "Scan Result : " + JSON.stringify(s));
 }
